f3cb39f661180b3d248b80b7ee86a06a7fea20eb
Dawson Reid <=>
2013-06-19 02:22:12 -0300

    Updated UI and added smooth scrolling.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,3 +2,13 @@
 window.onload = function(event) {
   $('#sl-background').css('top', $('.navbar').css('height'));
 };
+
+var animateTo = function(selector) {
+  var position;
+  if (selector === 'top') {
+    position = 0;
+  } else {
+    position = $(selector).offset().top - 95;
+  }
+  $('html, body').animate({scrollTop: position }, 1000);
+}
