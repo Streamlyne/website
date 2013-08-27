2073f626b9d67ef11e5494ad4d3a8a95fa4d8cf3
Dawson Reid <dreid93@gmail.com>
2013-08-27 13:10:14 -0300

    Updating to head out.

--- streamsite.py
+++ streamsite.py
@@ -1,2 +1,28 @@
 __author__ = 'dawson'
 
+from jinja2 import Template
+
+import os
+import shutil
+
+def compileCSS():
+    '''
+
+    '''
+    pass
+
+def compileJS():
+    '''
+
+    '''
+    pass
+
+def compileHTML():
+    '''
+
+    '''
+    pass
+
+compileCSS()
+compileJS()
+compileHTML()
