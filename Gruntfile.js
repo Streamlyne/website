434d8728ecbdcb06fe80fa339d95b38fe8420889
Todd Murphy <todd@streamlyne.co>
2014-04-24 19:26:32 -0300

    Improve and completed sales content with Todd and Jackie.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,8 +47,8 @@ module.exports = function(grunt) {
 
     connect: {
       options: {
-        port: 9000,
-        livereload: 35729,
+        port: 9001,
+        livereload: 35730,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
