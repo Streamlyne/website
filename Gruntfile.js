2fde03b007424cf99a6afa089a877a343388a150
babylemonade <jackie@streamlyne.co>
2014-08-27 10:48:59 -0300

    updated website content new screenshots alarms metrics scenarios descriptions and new pictures on each page

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
         port: 9001,
         livereload: 35730,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
